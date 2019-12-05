5d19339fef82e8373fbb5e03d1c3e5bc5567d617
gaozhiwei <gaozhiwei@k2data.com.cn>
2019-12-05 15:44:14 +0800

    ..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import moment from 'moment';
+import { Table } from 'antd';
+import 'antd/dist/antd.css'
+
 import './App.css';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+    render: (a,b) => { console.log(a,b); },
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
+console.log(moment('2019-08-15T15:51:21.582+08:00').format('YYYY-MM-DD hh:mm:ss'));
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 52,
+    address: 'Sidney No. 1 Lake Park',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table
+          columns={columns}
+          dataSource={data}
+        />
     </div>
   );
 }
